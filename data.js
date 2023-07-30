6d145f49ac625e5a00702407aebae2c82cf8c59b
Arim Seventh <arimeq@gmail.com>
2023-07-30 15:52:25 +0200

    extracted data to a separate file to improve maintainability

---
+++ data.js
@@ -0,0 +1,50 @@
+const Questions = [
+  {
+    q: "Pytanie 1",
+    a: [
+      {
+        text: "Nie jestem smutny / smutna ani przygnębiony / przygnębiona.",
+        score: 0,
+      },
+      { text: "Odczuwam często smutek, przygnębienie.", score: 1 },
+      {
+        text: "Przeżywam stale smutek, przygnębienie i nie mogę uwolnić się od tych przeżyć.",
+        score: 2,
+      },
+      {
+        text: "Jestem stale tak smutny / smutna i nieszczęśliwy / nieszczęśliwa, że jest to nie do wytrzymania.",
+        score: 3,
+      },
+    ],
+  },
+  {
+    q: "Pytanie 2",
+    a: [
+      { text: "Nie przejmuję się zbytnio przyszłością.", score: 0 },
+      { text: "Często martwię się o przyszłość.", score: 1 },
+      {
+        text: "Obawiam się, że w przyszłości nic dobrego mnie nie czeka.",
+        score: 2,
+      },
+      {
+        text: "Czuję, że przyszłość jest beznadziejna i nic tego nie zmieni.",
+        score: 3,
+      },
+    ],
+  },
+  {
+    q: "Pytanie 3",
+    a: [
+      { text: "Sądzę, że nie popełniam większych zaniedbań.", score: 0 },
+      { text: "Sądzę, że czynię więcej zaniedbań niż inni.", score: 1 },
+      {
+        text: "Kiedy spoglądam na to, co robiłem / robiłam, widzę mnóstwo błędów i zaniedbań.",
+        score: 2,
+      },
+      {
+        text: "Jestem zupełnie niewydolny / niewydolna i wszystko robię źle.",
+        score: 3,
+      },
+    ],
+  },
+];
